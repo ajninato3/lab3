ffb9b91c1e8d664cf403d948277f7b1ffbcd977b
ajninato3 <162399422+ajninato3@users.noreply.github.com>
2024-03-06 11:13:01 +0800

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,17 +1,11 @@
 <?php
     
     $sname= "localhost";
-
     $uname= "root";
-
     $password = "";
-
     $db_name = "IPT101";
-
     $port = 3307;
-
     $conn = mysqli_connect($sname, $uname, $password, $db_name, $port);
-
     if (!$conn) {
         echo "Connection failed!";
     }
